efa22fa505c38b147719e051d42a78a4333a618f
lalalaura022 <80111740+lalalaura022@users.noreply.github.com>
2023-01-06 15:07:37 +0200

    Add files via upload

---
+++ ProiectFinal/ProiectFinal/Models/Constants/UserRoleType.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+
+namespace ProiectFinal.Models.Constants
+{
+    public class UserRoleType
+    {
+        public static readonly string Admin = "Admin";
+        public static readonly string User = "User";
+    }
+}
---
+++ ProiectFinal/ProiectFinal/Models/DTOs/RegisterUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProiectFinal.Models.DTOs
+{
+    public class RegisterUserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
